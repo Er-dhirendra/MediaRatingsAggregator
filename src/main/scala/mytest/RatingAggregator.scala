d369732da9aa81df3304ab3063800d351d373391
dkashyap1090 <dkashyap1090@outlook.com>
2025-12-20 00:45:36 +0530

    Fixed the code

--- src/main/scala/mytest/RatingAggregator.scala
+++ src/main/scala/mytest/RatingAggregator.scala
@@ -9,30 +9,40 @@ import scala.collection.concurrent.TrieMap
 case class Rating(viewerId: String, score: Double)
 
 // Case class for Aggregate metrics
-case class Aggregates(averageRating: Double, reach: Int, engagementScore: Double)
+case class Aggregates(
+    averageRating: Double,
+    reach: Int,
+    engagementScore: Double
+)
 
 // Messages for the Actor
 sealed trait RatingMessage
 case class AddRating(showName: String, rating: Rating) extends RatingMessage
-case class ComputeAggregates(showName: String, replyTo: ActorRef[Option[Aggregates]]) extends RatingMessage
+case class ComputeAggregates(
+    showName: String,
+    replyTo: ActorRef[Option[Aggregates]]
+) extends RatingMessage
 
 // RatingAggregator Actor
-class RatingAggregator(context: ActorContext[RatingMessage]) extends AbstractBehavior[RatingMessage](context) {
+class RatingAggregator(context: ActorContext[RatingMessage])
+    extends AbstractBehavior[RatingMessage](context) {
   // Store ratings per show using an immutable data structure
-  private val ratingsByShow: TrieMap[String, Vector[Rating]] = TrieMap.empty[String, Vector[Rating]]
+  private val ratingsByShow: TrieMap[String, Vector[Rating]] =
+    TrieMap.empty[String, Vector[Rating]]
 
-  override def onMessage(msg: RatingMessage): Behavior[RatingMessage] = msg match {
-    case AddRating(showName, rating) =>
-      ratingsByShow.updateWith(showName) {
-        case Some(existingRatings) => Some(existingRatings :+ rating)
-        case None => Some(Vector(rating))
-      }
-      Behaviors.same
+  override def onMessage(msg: RatingMessage): Behavior[RatingMessage] =
+    msg match {
+      case AddRating(showName, rating) =>
+        ratingsByShow.updateWith(showName) {
+          case Some(existingRatings) => Some(existingRatings :+ rating)
+          case None                  => Some(Vector(rating))
+        }
+        Behaviors.same
 
-    case ComputeAggregates(showName, replyTo) =>
-      replyTo ! computeAggregates(showName)
-      Behaviors.same
-  }
+      case ComputeAggregates(showName, replyTo) =>
+        replyTo ! computeAggregates(showName)
+        Behaviors.same
+    }
 
   private def computeAggregates(showName: String): Option[Aggregates] = {
     ratingsByShow.get(showName).map { ratings =>
@@ -49,9 +59,10 @@ class RatingAggregator(context: ActorContext[RatingMessage]) extends AbstractBeh
       }
     }
   }
-  
+
 }
 
 object RatingAggregator {
-  def apply(): Behavior[RatingMessage] = Behaviors.setup(context => new RatingAggregator(context))
+  def apply(): Behavior[RatingMessage] =
+    Behaviors.setup(context => new RatingAggregator(context))
 }
--- src/test/scala/RatingAggregatorSpec.scala
+++ src/test/scala/RatingAggregatorSpec.scala
@@ -1,27 +1,31 @@
-import akka.actor.testkit.typed.scaladsl.{ActorTestKit, TestProbe}
+import akka.actor.testkit.typed.scaladsl.{
+  ActorTestKit,
+  ScalaTestWithActorTestKit,
+  TestProbe
+}
 import akka.actor.typed.ActorRef
-import mytest.{AddRating, Aggregates, ComputeAggregates, Rating, RatingAggregator, RatingMessage}
-import org.scalatest.BeforeAndAfterAll
+import mytest.{
+  AddRating,
+  Aggregates,
+  ComputeAggregates,
+  Rating,
+  RatingAggregator,
+  RatingMessage
+}
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.wordspec.AnyWordSpecLike
 
-class RatingAggregatorSpec extends AnyWordSpec
-  with BeforeAndAfterAll
-  with Matchers {
-
-  // Initialize the Akka TestKit
-  val testKit = ActorTestKit()
-
-  // Clean up resources after all tests
-  override def afterAll(): Unit = {
-    testKit.shutdownTestKit()
-  }
+class RatingAggregatorSpec
+    extends ScalaTestWithActorTestKit
+    with AnyWordSpecLike
+    with Matchers {
 
   "RatingAggregator" should {
 
     "add ratings and compute aggregates correctly" in {
       // Create the RatingAggregator actor
-      val ratingAggregator: ActorRef[RatingMessage] = testKit.spawn(RatingAggregator(), "ratingAggregator")
+      val ratingAggregator: ActorRef[RatingMessage] =
+        spawn(RatingAggregator(), "ratingAggregator")
 
       // Test data
       val showName = "Show1"
@@ -29,16 +33,15 @@ class RatingAggregatorSpec extends AnyWordSpec
         Rating("viewer1", 4.0),
         Rating("viewer2", 3.0),
         Rating("viewer3", 5.0),
-        Rating("viewer1", 4.5) 
+        Rating("viewer1", 4.5)
       )
 
-      // Add ratings
+      // Add ratings with proper timing
       ratings.foreach { rating =>
         ratingAggregator ! AddRating(showName, rating)
       }
 
-      // Create a TestProbe to receive the aggregates
-      val probe = testKit.createTestProbe[Option[Aggregates]]()
+      val probe = createTestProbe[Option[Aggregates]]()
 
       // Send ComputeAggregates message
       ratingAggregator ! ComputeAggregates(showName, probe.ref)
@@ -48,16 +51,19 @@ class RatingAggregatorSpec extends AnyWordSpec
       val expectedReach = 3 // Unique viewers: viewer1, viewer2, viewer3
       val expectedEngagementScore = (expectedAverage * expectedReach) / 4
 
-      val expectedAggregates = Some(Aggregates(expectedAverage, expectedReach, expectedEngagementScore))
+      val expectedAggregates = Some(
+        Aggregates(expectedAverage, expectedReach, expectedEngagementScore)
+      )
       probe.expectMessage(expectedAggregates)
     }
 
     "return None for a show with no ratings" in {
       // Create the RatingAggregator actor
-      val ratingAggregator: ActorRef[RatingMessage] = testKit.spawn(RatingAggregator(), "ratingAggregatorEmpty")
+      val ratingAggregator: ActorRef[RatingMessage] =
+        spawn(RatingAggregator(), "ratingAggregatorEmpty")
 
       // Create a TestProbe to receive the aggregates
-      val probe = testKit.createTestProbe[Option[Aggregates]]()
+      val probe = createTestProbe[Option[Aggregates]]()
 
       // Send ComputeAggregates message for a non-existent show
       ratingAggregator ! ComputeAggregates("NonExistentShow", probe.ref)
